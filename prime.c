ed919c1183b077fb9d7e44cb1f09f4a7e5cf1cae
iz00 <iz00@users.noreply.github.com>
2023-02-24 17:25:09 -0300

    automated commit by check50 [check50=True]

--- prime.c
+++ prime.c
@@ -1,3 +1,5 @@
+// Prints all prime numbers in an interval
+
 #include <cs50.h>
 #include <stdio.h>
 
@@ -5,6 +7,7 @@ bool prime(int number);
 
 int main(void)
 {
+    // Gets a number equal to or higher than 1 to be the start point
     int min;
     do
     {
@@ -12,6 +15,7 @@ int main(void)
     }
     while (min < 1);
 
+    // Gets a number equal to or less than the minimum, to be the ending point
     int max;
     do
     {
@@ -19,6 +23,7 @@ int main(void)
     }
     while (min >= max);
 
+    // For each number of the interval, check if prime, print if true
     for (int i = min; i <= max; i++)
     {
         if (prime(i))
@@ -28,18 +33,25 @@ int main(void)
     }
 }
 
+// Checks for prime numbers, false if not, true otherwise
 bool prime(int number)
 {
-    if ((number != 2) && (number % 2 == 0))
+    // Returns false for even numbers (excluding 2), or for 1
+    if (((number != 2) && (number % 2 == 0)) || (number == 1))
     {
         return false;
     }
-    for (int i = 2; i < number; i++)
+
+    // Returns false for numbers that have a divisor (excluding 1 and themselves)
+    // There are no even divisors possible (since all numbers left are odd), and no divisors greather than the half of the number
+    int half = number / 2;
+    for (int i = 3; i < half; i += 2)
     {
         if (number % i == 0)
         {
             return false;
         }
     }
+
     return true;
 }
