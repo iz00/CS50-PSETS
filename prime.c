d5c9d77404323322d3918b99fff5f335cbd34aec
iz00 <iz00@users.noreply.github.com>
2023-11-06 16:43:13 -0300

    automated commit by check50 [check50=True]

--- prime.c
+++ prime.c
@@ -1,26 +1,53 @@
+// List all prime numbers in a range specified by the user
+
 #include <cs50.h>
 #include <stdio.h>
 
+int get_min(void);
+int get_max(int m);
+void tests_numbers(int mi, int ma);
 bool prime(int number);
 
 int main(void)
 {
-    int min;
+    // Prompt for min and max of the range
+    int min = get_min();
+    int max = get_max(min);
+
+    // Test each number in the range if prime, print number if true
+    tests_numbers(min, max);
+}
+
+// Prompt user for min of the range, check if positive
+int get_min(void)
+{
+    int n;
     do
     {
-        min = get_int("Minimum: ");
+        n = get_int("Minimum: ");
     }
-    while (min < 1);
+    while (n < 1);
+    return n;
+}
 
-    int max;
+// Prompt user for max of the range, check if greater than min
+int get_max(int m)
+{
+    int n;
     do
     {
-        max = get_int("Maximum: ");
+        n = get_int("Maximum: ");
     }
-    while (min >= max);
+    while (n <= m);
+    return n;
+}
 
-    for (int i = min; i <= max; i++)
+// Loop through all numbers between mi and ma, return is void, only print number if prime
+void tests_numbers(int mi, int ma)
+{
+    for (int i = mi; i <= ma; i++)
     {
+        // The number is passed to the function prime as an argument, to check if prime
         if (prime(i))
         {
             printf("%i\n", i);
@@ -28,20 +55,29 @@ int main(void)
     }
 }
 
+// Return true if number is prime, false otherwise
 bool prime(int number)
 {
-    int divisors = 0;
-    for (int i = 2; i < number; i++)
-    {
-        divisors += (number % i == 0) ? 1 : 0;
-    }
-
-    if (divisors == 0)
-    {
-        return true;
-    }
-    else
+    // 1 isn't prime, as well as even numbers, except for 2
+    if ((number == 1) || ((number % 2 == 0) && (number != 2)))
     {
         return false;
     }
+
+    // Get the max number necessary for testing in the loop
+    int max_test = number / 2 + 2;
+
+    // Starting from 3 until the half of the number + 2
+    // i gets incremented by 2, so it is always odd
+    for (int i = 3; i < max_test; i += 2)
+    {
+        // Test if the number is divisible by i, if true return false
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    // If function reach here, the loop couldn't find a divisor, return true
+    return true;
 }
