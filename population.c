a02a55cc9a1b958d1d64c99597a24919422d013d
iz00 <iz00@users.noreply.github.com>
2023-11-06 10:31:30 -0300

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -1,25 +1,29 @@
+// Calculates number of years to get from start to end size of population
+
 #include <cs50.h>
 #include <stdio.h>
 
 int get_start_size(void);
-int get_end_size(int a);
+int get_end_size(int s);
 int calculate_years(int s, int e);
 
 int main(void)
 {
-    // TODO: Prompt for start size
+    // Prompt for start size
     int start = get_start_size();
 
-    // TODO: Prompt for end size
+    // Prompt for end size
     int end = get_end_size(start);
 
-    // TODO: Calculate number of years until we reach threshold
+    // Calculate number of years until we reach threshold
     int years = calculate_years(start, end);
 
-    // TODO: Print number of years
+    // Print number of years
     printf("Years: %i\n", years);
 }
 
+// Prompts user for start size, checks if greater than or equal to 9
+// Because if less than 9, the population will stagnate
 int get_start_size(void)
 {
     int n;
@@ -31,24 +35,27 @@ int get_start_size(void)
     return n;
 }
 
-int get_end_size(int a)
+// Prompts user for end size, checks if greater than or equal to start size
+int get_end_size(int s)
 {
     int n;
     do
     {
         n = get_int("End size: ");
     }
-    while (n < a);
+    while (n < s);
     return n;
 }
 
+// Calculates number of years from start size to end size in a loop
 int calculate_years(int s, int e)
 {
-    int i = 0;
+    int y = 0;
     while (e > s)
     {
+        // In a year, n / 3 llamas are born and n / 4 llamas pass away
         s += s / 3 - s / 4;
-        i++;
+        y++;
     }
-    return i;
+    return y;
 }
