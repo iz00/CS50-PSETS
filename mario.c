b7b320756f9ef08930ed6d07dab2bb225e9dbb4e
iz00 <iz00@users.noreply.github.com>
2023-11-06 19:13:22 -0300

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -3,6 +3,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Definitions of min and max height of pyramid
 #define MIN_HEIGHT 1
 #define MAX_HEIGHT 8
 
@@ -12,11 +13,14 @@ void print_pyramid(int n);
 
 int main(void)
 {
+    // Prompt for height of pyramid
     int height = get_height();
 
+    // Print pyramid with height
     print_pyramid(height);
 }
 
+// Prompt user for height, check if greater than MIN and less than MAX
 int get_height(void)
 {
     int n;
@@ -29,6 +33,7 @@ int get_height(void)
     return n;
 }
 
+// Prints determined character c, n times
 void print_char_times(char c, int n)
 {
     for (int i = 0; i < n; i++)
@@ -37,16 +42,20 @@ void print_char_times(char c, int n)
     }
 }
 
+// Print pyramid with height n
 void print_pyramid(int n)
 {
+    // Loop height times, i starts at 1 because it is the number of # printed each line
     for (int i = 1; i <= n; i++)
     {
+        // The number of empty spaces printed in every line is the height - i
         int num_empty_spaces = n - i;
 
         print_char_times(' ', num_empty_spaces);
 
         print_char_times('#', i);
 
+        // Create a new line
         printf("\n");
     }
 }
