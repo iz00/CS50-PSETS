6a6ef9039efd3519b73ea462cf1d01a36bb50bdb
iz00 <iz00@users.noreply.github.com>
2023-11-07 20:13:45 -0300

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,4 +1,4 @@
-// Validate a card number with luhn's algorithm and check card brand
+// Validate a card number with Luhn's algorithm and check card brand
 
 #include <cs50.h>
 #include <stdio.h>
@@ -6,7 +6,6 @@
 int card_digits_amount(long n);
 long power_base_ten(int times);
 int calculate_luhn_sum(long n, int digits);
-bool validate_checksum(int s);
 string validate_card_check_type(long n, int s, int digits);
 
 int main(void)
@@ -14,12 +13,16 @@ int main(void)
     // Prompt user for card number, long because can't fit in int
     long number = get_long("Number: ");
 
+    // Calculate amount of digits in card number
     int digits_amount = card_digits_amount(number);
 
+    // Calculate Luhn's algorithm sum of card number
     int sum = calculate_luhn_sum(number, digits_amount);
 
+    // Get string of validation or type of card
     string card_type = validate_card_check_type(number, sum, digits_amount);
 
+    // Print card type, or invalid
     printf("%s\n", card_type);
 }
 
@@ -28,6 +31,7 @@ int card_digits_amount(long n)
 {
     int digits = 0;
 
+    // Current last digit of card number is removed in every iteration, and digits increased
     while (n > 0)
     {
         digits++;
@@ -36,6 +40,7 @@ int card_digits_amount(long n)
     return digits;
 }
 
+// Calculate 10 to the power of times, result is digit 1 followed by times digits 0
 long power_base_ten(int times)
 {
     long n = 1;
@@ -46,6 +51,7 @@ long power_base_ten(int times)
     return n;
 }
 
+// Calculate Luhn's algorithm
 int calculate_luhn_sum(long n, int digits)
 {
     int s = 0;
@@ -53,12 +59,15 @@ int calculate_luhn_sum(long n, int digits)
 
     for (int i = 0; i < digits; i++)
     {
+        // Variable digit is digit of card number relative to iteration, starting from right
         digit = n % power_base_ten(i + 1) / power_base_ten(i);
 
+        // If iteration pair, s receives the digit
         if (i % 2 == 0)
         {
             s += digit;
         }
+        // Else, receives the digits of the digit * 2
         else
         {
             digit *= 2;
@@ -68,18 +77,22 @@ int calculate_luhn_sum(long n, int digits)
     return s;
 }
 
+// Return validation (invalid if it is) and type (if valid) of card
 string validate_card_check_type(long n, int s, int digits)
 {
+    // Card invalid if last digit of sum is not 0
     if (s % 10 != 0)
     {
         return "INVALID";
     }
 
+    // Get first and second digit of card number
     int first_digit = n / power_base_ten(digits - 1);
     int second_digit = n / power_base_ten(digits - 2) % 10;
 
     switch (first_digit)
     {
+        // Return American Express if all following conditions are valid
         case 3:
             if (second_digit == 4 || second_digit == 7)
             {
@@ -90,6 +103,7 @@ string validate_card_check_type(long n, int s, int digits)
             }
             break;
 
+        // Return MasterCard if all following conditions are valid
         case 5:
             if (second_digit > 0 && second_digit < 6)
             {
@@ -100,6 +114,7 @@ string validate_card_check_type(long n, int s, int digits)
             }
             break;
 
+        // Return Visa if all following conditions are valid
         case 4:
             if (digits == 13 || digits == 16)
             {
@@ -107,5 +122,6 @@ string validate_card_check_type(long n, int s, int digits)
             }
             break;
     }
+    // If not check validation for brands, is invalid
     return "INVALID";
 }
