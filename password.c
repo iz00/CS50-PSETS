71462e473fe637510778aa6eff5043d6bda09797
iz00 <iz00@users.noreply.github.com>
2023-11-11 01:10:15 -0300

    automated commit by check50 [check50=True]

--- password.c
+++ password.c
@@ -1,6 +1,4 @@
 // Check that a password has at least one lowercase letter, uppercase letter, number and symbol
-// Practice iterating through a string
-// Practice using the ctype library
 
 #include <cs50.h>
 #include <ctype.h>
@@ -11,7 +9,10 @@ bool valid(string password);
 
 int main(void)
 {
+    // Prompt user for password
     string password = get_string("Enter your password: ");
+
+    // Check if password valid, and print message accordingly
     if (valid(password))
     {
         printf("Your password is valid!\n");
@@ -22,30 +23,38 @@ int main(void)
     }
 }
 
-// TODO: Complete the Boolean function below
+// Return true or false for validation of password
 bool valid(string password)
 {
+    // Bool variable for characteristics password must have, initially all false
     bool has_lower = false, has_upper = false, has_number = false, has_symbol = false;
+
+    // Loop through password, len is calculated first for efficiency
     for (int i = 0, len = strlen(password); i < len; i++)
     {
+        // Check if password character is lowercase, if true change variable
         if (islower(password[i]))
         {
             has_lower = true;
         }
+        // Check if password character is uppercase, if true change variable
         if (isupper(password[i]))
         {
             has_upper = true;
         }
+        // Check if password character is a number, if true change variable
         if (isdigit(password[i]))
         {
             has_number = true;
         }
+        // Check if password character is a symbol, if true change variable
         if (ispunct(password[i]))
         {
             has_symbol = true;
         }
     }
 
+    // Return true if password has all tests for validation as true
     if (has_lower && has_upper && has_number && has_symbol)
     {
         return true;
