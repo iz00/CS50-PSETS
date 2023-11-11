439c6f34ba2eb8b2beeced07bba83cf0e18c597b
iz00 <iz00@users.noreply.github.com>
2023-11-11 11:53:14 -0300

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -4,41 +4,64 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string s);
+int count_words(string s);
+int count_sentences(string s);
+
 int main(void)
 {
     // Prompt user for text
     string text = get_string("Text: ");
 
     // Define three variables, letters count, words count, and sentences count
-    int letters_count = 0, words_count = 1, sentences_count = 0;
+    int letters_count = count_letters(text);
+    int words_count = count_words(text);
+    int sentences_count = count_sentences(text);
 
-    // Loop through the entire text
-    for (int i = 0, len = strlen(text); i < len; i++)
+    float L = letters_count / (float) words_count * 100.0;
+    float S = sentences_count / (float) words_count * 100.0;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    printf("Grade %i\n", index);
+}
+
+int count_letters(string s)
+{
+    int n = 0;
+    // Use isalpha in every character, if is increment letters count
+    for (int i = 0, len = strlen(s); i < len; i++)
     {
-        // Use isalpha in every character, if is increment letters count
-        if(isalpha(text[i]))
+        if(isalpha(s[i]))
         {
-            letters_count++;
-        }
-
-        switch (text[i])
-        {
-            case ' ':
-                words_count++;
-                break;
-
-            case '.':
-            case '!':
-            case '?':
-                sentences_count++;
-                break;
+            n++;
         }
     }
+    return n;
+}
 
-float L = letters_count / (float) words_count * 100.0;
-float S = sentences_count / (float) words_count * 100.0;
+int count_words(string s)
+{
+    int n = 1;
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if ((s[i] == ' ') && (i != 0) && (i != len - 1) && (s[i - 1] != ' '))
+        {
+            n++;
+        }
+    }
+    return n;
+}
 
-int index = round(0.0588 * L - 0.296 * S - 15.8);
-
-printf("Grade %i\n", index);
+int count_sentences(string s)
+{
+    int n = 0;
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
+        {
+            n++;
+        }
+    }
+    return n;
 }
