f9788759b6e7354b92d8f0d0bd9d302555919dbb
iz00 <iz00@users.noreply.github.com>
2023-11-11 19:42:33 -0300

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,31 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool validate_argc(int c);
+bool validate_argument(string v);
+
 int main(int argc, string argv[])
 {
 
-    if (argc != 2)
+    bool command_argument_valid = validate_argc(argc);
+
+    if (command_argument_valid)
+    {
+        command_argument_valid = validate_argument(argv[1]);
+    }
+
+    if (! command_argument_valid)
     {
         printf("Usage: ./caesar key\n");
         return 1;
-    }
-
-    for (int i = 0, len = strlen(argv[1]); i < len; i++)
-    {
-        if (isdigit(argv[1][i]) == 0)
-        {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
     }
 
     int key = atoi(argv[1]);
-
-    if (key < 0)
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
 
     string text = get_string("plaintext:  ");
 
@@ -36,15 +31,44 @@ int main(int argc, string argv[])
     {
         if (isupper(text[i]))
         {
-            text[i] = ((text[i] - 'A') + (key)) % 26 + 'A';
+            text[i] = (text[i] - 'A' + key) % 26 + 'A';
         }
         if (islower(text[i]))
         {
-            text[i] = ((text[i] - 'a') + (key)) % 26 + 'a';
+            text[i] = (text[i] - 'a' + key) % 26 + 'a';
         }
     }
 
     printf("ciphertext: %s\n", text);
 
     return 0;
+}
+
+bool validate_argc(int c)
+{
+    if (c != 2)
+    {
+        return false;
+    }
+    return true;
+}
+
+
+bool validate_argument(string v)
+{
+    for (int i = 0, len = strlen(v); i < len; i++)
+    {
+        if (isdigit(v[i]) == 0)
+        {
+            return false;
+        }
+    }
+
+    if (atoi(v) < 0)
+    {
+        printf("Usage: ./caesar key\n");
+        return false;
+    }
+
+    return true;
 }
