ec7892e99fbc0be9a8d5236e63ea56f69aeb974c
iz00 <iz00@users.noreply.github.com>
2023-11-11 22:36:48 -0300

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -1,7 +1,11 @@
+// Encode a message per a mapping of the alphabet letters to letters in a key input by the user
+
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+#define ALPHABET_LENGTH 26
 
 string validate_argc(int c);
 string validate_argument(string v);
@@ -9,6 +13,7 @@ void print_ciphertext(string text, string key);
 
 int main(int argc, string argv[])
 {
+    // Variable command_argument_validation is valid if command line argument is valid, else is custom message
     string command_argument_validation = validate_argc(argc);
 
     if (!strcmp(command_argument_validation, "valid"))
@@ -16,6 +21,7 @@ int main(int argc, string argv[])
         command_argument_validation = validate_argument(argv[1]);
     }
 
+    // If command line argument not valid, print the corresponding message and return 1 for error
     if (strcmp(command_argument_validation, "valid"))
     {
         printf("%s\n", command_argument_validation);
@@ -25,12 +31,13 @@ int main(int argc, string argv[])
     // Prompt user for plaintext
     string plaintext = get_string("plaintext:  ");
 
-    // Encode plaintext with the key, converted from string to int, and print ciphertext
+    // Encode plaintext with the key, and print ciphertext
     print_ciphertext(plaintext, argv[1]);
 
     return 0;
 }
 
+// Validate amount of arguments, if not 2, return usage message
 string validate_argc(int c)
 {
     if (c != 2)
@@ -40,21 +47,26 @@ string validate_argc(int c)
     return "valid";
 }
 
+// Validate argument, if passed validation of argc
 string validate_argument(string v)
 {
     int len = strlen(v);
-    if (len != 26)
+    // The length of argument has to be the same as the alphabet, for correct mapping
+    if (len != ALPHABET_LENGTH)
     {
         return "Key must contain 26 characters.";
     }
 
+    // Loop through argument to get each character through i
     for (int i = 0; i < len; i++)
     {
+        // All characters of argument have to be letters
         if (!isalpha(v[i]))
         {
             return "Key must only contain alphabetic characters.";
         }
 
+        // Check if there are two equal letters in argument, which can't happen for correct mapping
         for (int j = i + 1; j < len; j++)
         {
             if (tolower(v[i]) == tolower(v[j]))
@@ -66,6 +78,7 @@ string validate_argument(string v)
     return "valid";
 }
 
+// Encode the text with the key and print it
 void print_ciphertext(string text, string key)
 {
     printf("ciphertext: ");
@@ -73,6 +86,8 @@ void print_ciphertext(string text, string key)
     // Loop through text to get each character through i, len is calculated first for efficiency
     for (int i = 0, len = strlen(text); i < len; i++)
     {
+        // Get alphabetical index of letter, then map it to corresponding position in the key
+        // Then print the letter uppercase or lowercase depending on how it was before encoding
         if (isupper(text[i]))
         {
             printf("%c", toupper(key[text[i] - 'A']));
