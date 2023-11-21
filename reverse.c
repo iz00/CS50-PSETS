19c6448dfc3417f9742c2c93af9454fae3e328da
iz00 <iz00@users.noreply.github.com>
2023-11-20 21:47:55 -0300

    automated commit by check50 [check50=True]

---
+++ reverse.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "wav.h"
+
+int check_format(WAVHEADER header);
+int get_block_size(WAVHEADER header);
+
+int main(int argc, char *argv[])
+{
+    // Ensure proper usage
+    // TODO #1
+    if (argc != 3)
+    {
+        printf("Usage: ./reverse input.wav output.wav\n");
+        return 1;
+    }
+
+    // Open input file for reading
+    // TODO #2
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // Read header
+    // TODO #3
+    WAVHEADER input_header;
+    fread(&input_header, sizeof(WAVHEADER), 1, input);
+
+    // Use check_format to ensure WAV format
+    // TODO #4
+    if (!check_format(input_header))
+    {
+        printf("Input is not a WAV file.\n");
+        return 1;
+    }
+
+    // Open output file for writing
+    // TODO #5
+    FILE *output = fopen(argv[2], "w");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // Write header to file
+    // TODO #6
+
+    // Use get_block_size to calculate size of block
+    // TODO #7
+
+    // Write reversed audio to file
+    // TODO #8
+
+    return 0;
+}
+
+int check_format(WAVHEADER header)
+{
+    char signature_format[] = {'W', 'A', 'V', 'E'};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (signature_format[i] != header.format[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int get_block_size(WAVHEADER header)
+{
+    // TODO #7
+    return 0;
+}
