29a212ae88a929e2f3b56067ed9ef38aba66dec7
iz00 <iz00@users.noreply.github.com>
2023-11-20 21:51:22 -0300

    automated commit by check50 [check50=True]

--- reverse.c
+++ reverse.c
@@ -50,6 +50,7 @@ int main(int argc, char *argv[])
 
     // Write header to file
     // TODO #6
+    fwrite(&input_header, sizeof(WAVHEADER), 1, output);
 
     // Use get_block_size to calculate size of block
     // TODO #7
