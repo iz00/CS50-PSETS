e8d8ffbaf9437d4e6d14a3d844ceff3996f96053
iz00 <iz00@users.noreply.github.com>
2023-11-20 22:22:51 -0300

    automated commit by check50 [check50=True]

--- reverse.c
+++ reverse.c
@@ -30,12 +30,14 @@ int main(int argc, char *argv[])
     // TODO #3
     WAVHEADER input_header;
     fread(&input_header, sizeof(WAVHEADER), 1, input);
+    long input_position = ftell(input);
 
     // Use check_format to ensure WAV format
     // TODO #4
     if (!check_format(input_header))
     {
         printf("Input is not a WAV file.\n");
+        fclose(input);
         return 1;
     }
 
@@ -45,18 +47,38 @@ int main(int argc, char *argv[])
     if (input == NULL)
     {
         printf("Could not open file.\n");
+        fclose(input);
         return 1;
     }
 
-    // Write header to file
+    // Write header to output file
     // TODO #6
     fwrite(&input_header, sizeof(WAVHEADER), 1, output);
 
     // Use get_block_size to calculate size of block
     // TODO #7
+    int block_size = get_block_size(input_header);
 
     // Write reversed audio to file
     // TODO #8
+    BYTE buffer[block_size];
+
+    while (fread(buffer, sizeof(BYTE), block_size, input))
+    {
+        continue;
+    }
+
+    fseek(input, block_size * (-1), ftell(input));
+
+    while (ftell(input) != input_position)
+    {
+        fread(buffer, sizeof(BYTE), block_size, input);
+        fseek(input, block_size * (-2), ftell(input));
+        fwrite(buffer, sizeof(BYTE), block_size, output);
+    }
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
@@ -77,6 +99,5 @@ int check_format(WAVHEADER header)
 
 int get_block_size(WAVHEADER header)
 {
-    // TODO #7
-    return 0;
+    return header.numChannels * (header.bitsPerSample / 8);
 }
