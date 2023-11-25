2bdb64ce0ef6fdaafd02264be33e3a788b3d65d6
iz00 <iz00@users.noreply.github.com>
2023-11-25 11:13:28 -0300

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -17,7 +17,7 @@ typedef struct node
 } node;
 
 // Number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 17576;
 
 // Hash table
 node *table[N];
@@ -33,7 +33,7 @@ bool check(const char *word)
     // Set pointer to beggining of linked list at corresponding index in table, then iterates over it
     for (node *ptr = table[index]; ptr != NULL; ptr = ptr->next)
     {
-        // If pointer points to node that contains word, case insensitive, word is in dictionary
+        // If pointer points to node that contains word (case insensitive), word is in dictionary
         if (!strcasecmp(ptr->word, word))
         {
             return true;
@@ -43,14 +43,15 @@ bool check(const char *word)
     return false;
 }
 
-// Hashes word to a number
+// Hash word to a number
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // return toupper(word[0]) - 'A';
+    return (toupper(word[0]) - 'A') * 26 * 26 + (toupper(word[1]) - 'A') * 26 + toupper(word[2]) - 'A';
 }
 
-// Loads dictionary into memory, returning true if successful, else false
+// Load dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     FILE *dict = fopen(dictionary, "r");
@@ -59,12 +60,12 @@ bool load(const char *dictionary)
         return false;
     }
 
-
-
     char word[LENGTH + 1];
 
-    while(fscanf(dict, "%s", word) != EOF)
+    // Read word by word in file, until no more words found
+    while (fscanf(dict, "%s", word) != EOF)
     {
+        // Allocate node for word and hash it
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
@@ -74,10 +75,9 @@ bool load(const char *dictionary)
 
         int index = hash(word);
 
+        // Put word in node, and insert it at beggining of linked list in corresponding index of table
         strcpy(n->word, word);
-
         n->next = table[index];
-
         table[index] = n;
 
         words_amount++;
@@ -87,21 +87,29 @@ bool load(const char *dictionary)
     return true;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+// Return number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-        return words_amount;
+    return words_amount;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
+// Unload dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
+    // Iterate over table
     for (int i = 0; i < N; i++)
     {
+        // Iterate over linked list of table's index, freeing each node
         while (table[i] != NULL)
         {
             node *ptr = table[i]->next;
+
             free(table[i]);
+            if (table[i] != NULL)
+            {
+                return false;
+            }
+
             table[i] = ptr;
         }
     }
