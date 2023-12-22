746ef9776d3cc77c860adbf281894185912e74a4
iz00 <iz00@users.noreply.github.com>
2023-12-22 14:29:03 -0300

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -18,10 +18,26 @@ def main():
         words_count = len(words)
         sentences_count = text.count(".") + text.count("!") + text.count("?")
 
-    L = letters_count / words_count * 100
-    S = sentences_count / words_count * 100
+    index = calculate_index(letters_count, words_count, sentences_count)
 
-    print(round(0.0588 * L - 0.296 * S - 15.8))
+    print(get_grade(index))
+
+
+def calculate_index(letters, words, sentences):
+    L = letters / words * 100
+    S = sentences / words * 100
+
+    return round(0.0588 * L - 0.296 * S - 15.8)
+
+
+def get_grade(n):
+    if n < 1:
+        return "Before grade 1"
+
+    if n > 16:
+        return "Grade 16+"
+
+    return f"Grade {n}"
 
 
 main()
