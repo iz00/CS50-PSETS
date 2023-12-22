82eb538a6a21f8fc2c63d8addfb9f468be52ffc1
iz00 <iz00@users.noreply.github.com>
2023-12-22 00:01:59 -0300

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -1,8 +1,9 @@
 from sys import exit
 
+
 def main():
     card_number = input("Number: ").replace(" ", "").replace("-", "").replace(".", "")
-    if (not card_number.isdecimal()):
+    if not card_number.isdecimal():
         exit("INVALID")
 
     card_number_sum = 0
@@ -17,13 +18,17 @@ def main():
     if card_number_sum % 10 != 0:
         print("INVALID")
     else:
-        if len(card_number) == 15 and card_number[:2] in ["34", "37"]:
-            print("AMEX")
-        elif len(card_number) == 16 and card_number[:2] in ["51", "52", "53", "54", "55"]:
-            print("MASTERCARD")
-        elif (len(card_number) == 13 or len(card_number) == 16) and card_number[0] == "4":
-            print("VISA")
+        if len(card_number) == 15:
+            if card_number[:2] in ["34", "37"]:
+                print("AMEX")
+        elif len(card_number) == 16:
+            if card_number[:2] in ["51", "52", "53", "54", "55"]:
+                print("MASTERCARD")
+        elif len(card_number) == 13 or len(card_number) == 16:
+            if card_number[0] == "4":
+                print("VISA")
         else:
             print("INVALID")
+
 
 main()
