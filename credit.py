daba4ad1b6fb14ca96b3e031986fa7e1e4f7e3c3
iz00 <iz00@users.noreply.github.com>
2023-12-22 00:04:50 -0300

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -18,15 +18,13 @@ def main():
     if card_number_sum % 10 != 0:
         print("INVALID")
     else:
-        if len(card_number) == 15:
-            if card_number[:2] in ["34", "37"]:
-                print("AMEX")
-        elif len(card_number) == 16:
-            if card_number[:2] in ["51", "52", "53", "54", "55"]:
-                print("MASTERCARD")
-        elif len(card_number) == 13 or len(card_number) == 16:
+        if len(card_number) in [13, 15, 16]:
             if card_number[0] == "4":
                 print("VISA")
+            if card_number[:2] in ["34", "37"]:
+                print("AMEX")
+            if card_number[:2] in ["51", "52", "53", "54", "55"]:
+                print("MASTERCARD")
         else:
             print("INVALID")
 
