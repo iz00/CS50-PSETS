e9fb9259128bede4ce51b6aeeaf31ff646ab851f
iz00 <iz00@users.noreply.github.com>
2023-12-22 00:00:04 -0300

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -21,7 +21,7 @@ def main():
             print("AMEX")
         elif len(card_number) == 16 and card_number[:2] in ["51", "52", "53", "54", "55"]:
             print("MASTERCARD")
-        elif (len(card_number) == 13 or len(card_number) == 15) and card_number[0] == "4":
+        elif (len(card_number) == 13 or len(card_number) == 16) and card_number[0] == "4":
             print("VISA")
         else:
             print("INVALID")
