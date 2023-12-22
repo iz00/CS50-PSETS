e25a081d52be7f9421cbdbcae54e1a160d560182
iz00 <iz00@users.noreply.github.com>
2023-12-22 14:40:46 -0300

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -1,36 +1,51 @@
+# Calculate approximate grade level needed to comprehend some text, based in Coleman-Liau index
+
 def main():
     text = input("Text: ")
 
     words = text.split()
-
+    # Join the words, separated by spaces, to get rid of unnecessary spaces from user input
     text = " ".join(words)
 
-    letters_count = 0
+    letters_count = count_letters(text)
 
-    for c in text:
-        if c.isalpha():
-            letters_count += 1
-
+    # If there are no letters, there are also no words or sentences
     if letters_count == 0:
-        words_count = 0
-        sentences_count = 0
+        words_count = sentences_count = 0
     else:
+        # Get amount of words and of sentences, defined by ., ! or ?
         words_count = len(words)
         sentences_count = text.count(".") + text.count("!") + text.count("?")
 
     index = calculate_index(letters_count, words_count, sentences_count)
 
-    print(get_grade(index))
+    print(define_grade(index))
 
 
+# Calculate amount of letters in t
+def count_letters(t):
+    letters = 0
+
+    # Check each character if is alphabetical, if it is, increment counter
+    for c in t:
+        if c.isalpha():
+            letters += 1
+
+    return letters
+
+
+# Calculate index of Coleman-Liau formula
 def calculate_index(letters, words, sentences):
+    # Calculate amount of letters L and sentences S in 100 words
     L = letters / words * 100
     S = sentences / words * 100
 
+    # Round the result of formula applied
     return round(0.0588 * L - 0.296 * S - 15.8)
 
 
-def get_grade(n):
+# Check grade through index
+def define_grade(n):
     if n < 1:
         return "Before Grade 1"
 
