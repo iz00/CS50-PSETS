1760dc4de2f53b423e4886a90c5e9f0367e419f0
iz00 <iz00@users.noreply.github.com>
2023-12-22 00:33:29 -0300

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -6,28 +6,39 @@ def main():
     if not card_number.isdecimal():
         exit("INVALID")
 
-    card_number_sum = 0
+    card_number_sum = calculate_luhn_sum(card_number)
 
-    for digit in card_number[-2::-2]:
+    print(validate_card_check_type(card_number, card_number_sum))
+
+
+def calculate_luhn_sum(number):
+    s = 0
+
+    for digit in number[-2::-2]:
         digit = int(digit) * 2
-        card_number_sum += digit // 10 + digit % 10
+        s += digit // 10 + digit % 10
 
-    for digit in card_number[::-2]:
-        card_number_sum += int(digit)
+    for digit in number[::-2]:
+        s += int(digit)
 
-    if card_number_sum % 10 != 0:
-        print("INVALID")
+    return s
+
+
+def validate_card_check_type(number, s):
+    if s % 10 != 0:
+        return "INVALID"
     else:
-        card_digits = len(card_number)
-        first_two_digits = int(card_number[:2])
+        card_digits = len(number)
+        first_two_digits = int(number[:2])
+
         if card_digits == 15 and first_two_digits in [34, 37]:
-            print("AMEX")
+            return "AMEX"
         elif card_digits == 16 and first_two_digits in [51, 52, 53, 54, 55]:
-            print("MASTERCARD")
-        elif (card_digits == 13 or card_digits == 16) and card_number[0] == "4":
-            print("VISA")
+            return "MASTERCARD"
+        elif (card_digits == 13 or card_digits == 16) and number[0] == "4":
+            return "VISA"
         else:
-            print("INVALID")
+            return "INVALID"
 
 
 main()
