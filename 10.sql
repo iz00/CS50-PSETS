a1024b9aba208528bb9a105ee16b23290fb760c0
iz00 <iz00@users.noreply.github.com>
2023-12-27 14:28:40 -0300

    automated commit by check50 [check50=True]

---
+++ 10.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT(name) AS name
+  FROM people
+  JOIN directors
+    ON people.id = directors.person_id
+  JOIN movies
+    ON directors.person_id = movies.id
+  JOIN ratings
+    ON movies.id = ratings.movie_id
+ WHERE rating >= 9;
