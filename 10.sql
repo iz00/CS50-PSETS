98dd989bb522fb3f9dea060f1540cf78be9a3ccd
iz00 <iz00@users.noreply.github.com>
2023-12-27 14:36:59 -0300

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -1,9 +1,9 @@
-SELECT DISTINCT(name)
+SELECT DISTINCT(name) as name
   FROM people
   JOIN directors
     ON people.id = directors.person_id
   JOIN movies
-    ON directors.person_id = movies.id
+    ON directors.movie_id = movies.id
   JOIN ratings
     ON movies.id = ratings.movie_id
  WHERE rating >= 9;
