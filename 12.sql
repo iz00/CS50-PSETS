ca0c1be6b745bffac7ce93cf5ddac52b1ab21296
iz00 <iz00@users.noreply.github.com>
2023-12-27 16:15:08 -0300

    automated commit by check50 [check50=True]

---
+++ 12.sql
@@ -0,0 +1,11 @@
+SELECT title
+  FROM movies
+  JOIN stars
+    ON movies.id = stars.movie_id
+  JOIN people
+    ON stars.person_id = people.id
+ WHERE name IN (
+       'Bradley Cooper', 'Jennifer Lawrence'
+  )
+ GROUP BY title
+HAVING COUNT(title) > 1;
