2a2bb496fb55967ed7d5635463a6fc43d825ae7d
iz00 <isadoracontsost@gmail.com>
2024-02-03 15:46:21 -0300

    Add entry page feature for each entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,3 +7,20 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    entries = util.list_entries()
+    entries_capitalized = [entry.upper() for entry in entries]
+    title = title.upper()
+
+    if title not in entries_capitalized:
+        return render(request, "encyclopedia/error.html", {
+            "message": "invalid entry"
+        })
+    
+    title = entries[entries_capitalized.index(title)]
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": util.get_entry(title)
+    })
