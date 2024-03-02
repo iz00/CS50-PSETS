962fe6c66490eac4d8408751144c792cac6a9acd
iz00 <isadoracontsost@gmail.com>
2024-03-02 16:54:44 -0300

    Add send email functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,11 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
     document.querySelector('#compose').addEventListener('click', compose_email);
 
+    document.querySelector('#compose-form').addEventListener('submit', send_email);
+
     // By default, load the inbox
     load_mailbox('inbox');
 });
 
 function compose_email() {
+
+    document.querySelector('#compose-error').style.display = 'none';
 
     // Show compose view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
@@ -30,4 +34,30 @@ function load_mailbox(mailbox) {
 
     // Show the mailbox name
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(event) {
+
+    const error_message = document.querySelector('#compose-error');
+
+    fetch('/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+            recipients: document.querySelector('#compose-recipients').value,
+            subject: document.querySelector('#compose-subject').value,
+            body: document.querySelector('#compose-body').value
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        if ('error' in result) {
+            compose_email();
+            error_message.innerHTML = result.error;
+            error_message.style.display = 'block';
+        } else {
+            error_message.innerHTML = '';
+            load_mailbox('sent');
+        }
+    });
+    event.preventDefault();
 }
--- project3/settings.py
+++ project3/settings.py
@@ -119,5 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+APPEND_SLASH = False
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 STATIC_URL = '/static/'
